943ab246d414e1f4ac45fa6ae66fe39844efae66
Manoj Das <manoj@vrittechnologies.com>
2025-06-23 14:43:57 +0545

    models

--- diseases/models.py
+++ diseases/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-
-
-
-
-
-
 # ===============================
 # DISEASE MODEL
 # ===============================
